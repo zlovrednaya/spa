3b5f4334dcafd798cecde17adb73ea581c714ecc
daria <daria.gosteva@klientiks.ru>
2023-08-18 02:39:53 +0300

    spa done

--- resources/js/Index.jsx
+++ resources/js/Index.jsx
@@ -1,8 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import Main from "./Main";
  
-ReactDOM.render(
-  <Main/>, 
-  document.getElementById("root")
-);
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <Main/>
+  );
--- resources/js/Main.jsx
+++ resources/js/Main.jsx
@@ -1,19 +1,32 @@
 import React, { Component } from "react";
+import {
+    BrowserRouter,
+  Routes,
+  Route,
+  Link
+  } from "react-router-dom";
+import Home from "./content/Home";
+import Stuff from "./content/Stuff";
  
 class Main extends Component {
   render() {
     return (
-        <div>
-          <h1>Simple SPA</h1>
-          <ul className="header">
-            <li><a href="/">Home</a></li>
-            <li><a href="/stuff">Stuff</a></li>
-            <li><a href="/contact">Contact</a></li>
-          </ul>
-          <div className="content">
-             
-          </div>
-        </div>
+        <BrowserRouter>
+            <div>
+            <h1>Simple SPA</h1>
+            <ul className="header">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/stuff">Stuff</Link></li>
+            
+            </ul>
+            <div className="content">
+                <Routes>
+                    <Route exact path="/" element={<Home/>}/>
+                    <Route path="/stuff" element={<Stuff/>}/>
+                </Routes>
+            </div>
+            </div>
+      </BrowserRouter>
     );
   }
 }
