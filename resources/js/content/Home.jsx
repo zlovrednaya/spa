1f942a4f4e000b52fd4b6ffc4f3c8f0b5fb98373
daria <daria.gosteva@klientiks.ru>
2023-08-18 03:41:23 +0300

    css

--- resources/js/content/Home.jsx
+++ resources/js/content/Home.jsx
@@ -3,16 +3,9 @@ import React, { Component } from "react";
 class Home extends Component {
   render() {
     return (
-      <div>
-        <h2>HELLO</h2>
-        <p>Cras facilisis urna ornare ex volutpat, et
-        convallis erat elementum. Ut aliquam, ipsum vitae
-        gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-        metus nec massa. Maecenas hendrerit laoreet augue
-        nec molestie. Cum sociis natoque penatibus et magnis
-        dis parturient montes, nascetur ridiculus mus.</p>
- 
-        <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+      <div className="background-top">
+        <h2 className="slog">Investigating your business</h2>
+       
       </div>
     );
   }
