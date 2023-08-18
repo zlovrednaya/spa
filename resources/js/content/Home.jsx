08fd71759ecfb23be3396e0365f5db1a23ad76c1
daria <daria.gosteva@klientiks.ru>
2023-08-18 12:53:49 +0300

    css

--- resources/js/content/Home.jsx
+++ resources/js/content/Home.jsx
@@ -4,7 +4,15 @@ class Home extends Component {
   render() {
     return (
       <div className="background-top">
-        <h2 className="slog">Investigating your business</h2>
+        <div className="slog-container">
+         <h2 className="slog">Investigating your business</h2>
+        </div>
+        <div className="info-click info-button">
+          <span>Discover</span>
+        </div>
+        <div className="info-click info-go">
+          <div className="info-square"></div>
+        </div>
        
       </div>
     );
