511e8c8e7baf889e5a41f6bbca01823a7965d41f
daria <daria.gosteva@klientiks.ru>
2023-08-24 22:36:19 +0300

    scroll code

--- resources/js/content/Home.jsx
+++ resources/js/content/Home.jsx
@@ -5,8 +5,9 @@ import { useRef } from 'react';
 //same but with function
 export default function Home(){
   const ref = useRef(null);
-  const scrollToNextPage = () =>{
-    ref.current?.scrollIntoView({ behavior: 'smooth' });
+  const scrollToNextPage = (id) =>{
+    const elt = document.getElementById(id);
+    elt.scrollIntoView({ behavior: 'smooth' });
   };
 
   return (
@@ -15,7 +16,7 @@ export default function Home(){
         <div className="slog-container">
         <h2 className="slog">Investigating your business</h2>
         </div>
-        <div onClick={scrollToNextPage}>
+        <div onClick={()=>scrollToNextPage('two')}>
           <div className="info-click info-button">
             <span>Discover</span>
           </div>
@@ -25,7 +26,7 @@ export default function Home(){
         </div>
       
       </div>
-      <div className="page second-page" ref={ref}>
+      <div className="page second-page" ref={ref} id='two'>
         
         <div className="flex-img-txt">
           <div className="slogo-pg">
@@ -34,14 +35,14 @@ export default function Home(){
             
           </div>
           <div className="slog-small">Documents are in order</div>
-          <div className="circle-button" onClick={scrollToNextPage}></div>
+          <div className="circle-button" onClick={()=>scrollToNextPage('three')}></div>
           </div>
           <div className="img-order">
           
           </div>
         </div>
       </div>
-      <div className="page third-page">
+      <div className="page third-page" id='three'>
       <div className="flex-img-txt">
           <div className="slogo-rigth-pg">
             <div className="page-topic">
